12edbf9238e2ee3660f9c0c9254edf6bf2724dc8
20221170150052 <145566995+20221170150052@users.noreply.github.com>
2024-08-20 18:31:00 -0300

    Fez até o prefabs da fruta. Falta fazer o script

---
+++ Atividade JOGO 2BI  - Oficina/Assets/Pixel Adventure 1/Assets/Scripts/Player.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float Speed;
+    public float JumpForce;
+
+    private Rigidbody2D rig;
+    private Animator anim;
+    public bool isJumping;
+    public bool doubleJump;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+    void Move()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"),0f,0f);
+        transform.position += movement * Time.deltaTime * Speed;
+        if(Input.GetAxis("Horizontal")> 0f)
+        {
+            anim.SetBool("Bool Walk", true);
+            transform.eulerAngles = new Vector3(0f,0f,0f);
+        }
+        if(Input.GetAxis("Horizontal")< 0f)
+        {
+            anim.SetBool("Bool Walk", true);
+             transform.eulerAngles = new Vector3(0f,180f,0f);
+        }
+        if(Input.GetAxis("Horizontal") == 0f)
+        {
+            anim.SetBool("Bool Walk", false);
+        }
+    }
+    void Jump()
+    {
+
+    if(Input.GetButtonDown("Jump"))
+    {
+        if(!isJumping)
+        {
+            rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+            doubleJump = true;
+            anim.SetBool("Bool Jump", true);
+        }
+        else
+        {
+            if (doubleJump)
+            {
+                rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+                doubleJump = false;
+            }
+        }
+        
+    }    
+    }
+
+    void OnCollisionEnter2D(Collision2D colisor)
+    {
+        if(colisor.gameObject.layer == 6)
+        {
+            isJumping = false;
+            anim.SetBool("Bool Jump", false);
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D colisor )
+    {
+         if (colisor.gameObject.layer == 6)
+        {
+            isJumping = true;
+        }
+    }
+}

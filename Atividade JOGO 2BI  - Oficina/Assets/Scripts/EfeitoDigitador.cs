ced9eccf27845b96cfe08bdcec6a204ebfb06f90
20221170150052 <145566995+20221170150052@users.noreply.github.com>
2024-09-13 15:27:58 -0300

    Cut Scenes

---
+++ Atividade JOGO 2BI  - Oficina/Assets/Scripts/EfeitoDigitador.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+
+public class EfeitoDigitador : MonoBehaviour
+{
+    private TextMeshProUGUI componenteTexto;
+    private AudioSource _audioSource;
+    private string mensagemOriginal;
+    public bool imprimindo;
+    public float tempoEntreLetras = 0.08f;
+
+    private void Awake()
+    {
+        TryGetComponent(out componenteTexto);
+        TryGetComponent(out _audioSource);
+        mensagemOriginal = componenteTexto.text;
+        componenteTexto.text = "";
+    }
+
+    private void OnEnable()
+    {
+        imprimirMensagem (mensagemOriginal);
+    }
+
+    private void OnDisable()
+    {
+        componenteTexto.text = mensagemOriginal;
+        StopAllCoroutines();
+    }
+
+    public void imprimirMensagem(string mensagem)
+    {
+        if(gameObject.activeInHierarchy)
+        {
+            if (imprimindo) return;
+            StartCoroutine (LetraPorLetra(mensagem));
+        }
+    }
+    IEnumerator LetraPorLetra(string mensagem)
+    {
+        string msg = "";
+        foreach (var letra in mensagem)
+        {
+            msg += letra;
+            componenteTexto.text = msg;
+            _audioSourcer.Play();
+            yield return new WaitForSeconds(tempoEntreLetras);
+        }
+        imprimindo = false;
+        StopAllCoroutines();
+    }
+}

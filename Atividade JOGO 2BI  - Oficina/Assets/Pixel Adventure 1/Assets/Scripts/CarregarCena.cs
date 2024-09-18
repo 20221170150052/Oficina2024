84e6f34240453255a99b9d5db18915340c63d235
20221170150052 <145566995+20221170150052@users.noreply.github.com>
2024-09-18 14:09:44 -0300

    Acabei

--- Atividade JOGO 2BI  - Oficina/Assets/Pixel Adventure 1/Assets/Scripts/CarregarCena.cs
+++ Atividade JOGO 2BI  - Oficina/Assets/Pixel Adventure 1/Assets/Scripts/CarregarCena.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class CarregarCena : MonoBehaviour
-{
+{   
+    public string cenaParaCarregar;
     // Start is called before the first frame update
     void Start()
     {
-        
+        SceneManager.LoadScene(cenaParaCarregar);
     }
 
     // Update is called once per frame
